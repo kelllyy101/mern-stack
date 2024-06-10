0822319f814963299274b6c77841dec220454e6c
kelllyy101 <kellyom81@hotmail.com>
2024-06-10 12:51:44 +0100

    fetch workout data and outputted it in React template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fecthWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json() //array of objects each representing a workout
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+        fecthWorkouts();
+    }, []) //empty dependency array firing only once
+
     return (
-        <div className="home">  
-            <h2>Home</h2>
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map(workout => (
+                    <WorkoutDetails workout={workout} key={workout._id} />
+                ))}
+            </div>
         </div>
     )
 }
