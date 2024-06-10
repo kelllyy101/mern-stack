b0689a1f7fae8697456bd2de839f5a0b62a35aff
kelllyy101 <kellyom81@hotmail.com>
2024-06-10 15:55:58 +0100

    deleting exercise data

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,18 @@
+import { useWorkoutsContext} from '../hooks/useWorkoutsContext'
+
 const WorkoutDetails = ({workout}) => {
+    const { dispatch } = useWorkoutsContext();
+
+    const handleClick = async () => {
+        const response = await fetch('api/workouts/' + workout._id, {
+            method:'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok){
+            dispatch({type: 'DELETE_WORKOUT', payload:json})
+        }
+    }
     return (
         <div className="workout-details">
             <h4>{workout.title}
@@ -6,6 +20,7 @@ const WorkoutDetails = ({workout}) => {
             <p><strong>Loads (kg): {workout.weight} </strong></p>
             <p><strong>Reps: {workout.reps} </strong></p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 }
