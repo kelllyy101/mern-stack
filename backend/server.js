41f7c32a2c7253efb0d652eabd777e506a33fe02
kelllyy101 <kellyom81@hotmail.com>
2024-06-10 10:17:41 +0100

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config() //attach env varibale to process object
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express();
 
 //middleware
+app.use(express.json()) //looks if it has data sending to server, then it passes it and attaches it to the request obj to access in request handler
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
@@ -15,8 +17,16 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000', process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 
