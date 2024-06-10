84824e0110d6a7801a139b1395f2fead11608677
kelllyy101 <kellyom81@hotmail.com>
2024-06-10 10:28:58 +0100

    created workout model

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,22 +1,30 @@
 const express = require('express')
+const Workout = require('../models/workoutModels')
 
 const router = express.Router()
 
 //creating router, adding requst handlers, exporting router to use in api/workouts path 
 
-//get all workouts
+//GET all workouts
 router.get('/', (req, res) => {
     res.json({message: 'GET all workouts'})
 })
 
 // GET a single workout
-router.get('/:id', (res, req) => {
+router.get('/:id', (req, res) => {
     res.json({mssg: 'GET single workout'})
 })
 
 //POST a new workout
-router.post('/', (req, res) => {
-    res.json({mssg:'POST a new workout'})
+router.post('/', async (req, res) => {
+    const {title, reps, weight} = req.body
+
+    try {
+        const workout = await Workout.create({title, reps, weight})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 
 //DELETE a new workout
