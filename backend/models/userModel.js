f60b13dd2c3d06a48285de666790033f485810d6
kelllyy101 <kellyom81@hotmail.com>
2024-06-11 11:09:14 +0100

    functionality so users can login in

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const validator = require('validator')
 
+
 const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
@@ -46,6 +47,26 @@ userSchema.statics.signup = async function(email, password) {
 
     return user
 
+}
+
+//static login method
+userSchema.statics.login = async function(email, password){
+    if (!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Incorrect Email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+    return user
 }
 
 module.exports = mongoose.model('User', userSchema)
