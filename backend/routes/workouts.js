2cd468c5d541d77afe586381fe79f24a006c0619
kelllyy101 <kellyom81@hotmail.com>
2024-06-12 09:48:35 +0100

    protected api routes from non logged in users

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,32 @@
-const express = require('express')
+const express = require('express');
 const {createWorkout,
     getWorkouts,
     getWorkout,
     deleteWorkout,
-    updateWorkout} = require('../controllers/workoutControllers')
+    updateWorkout} = require('../controllers/workoutControllers');
 
-const router = express.Router()
+const requireAuth = require('../middleware/requireAuth');
 
-//creating router
+const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth);
 
 //GET all workouts
-router.get('/', getWorkouts)
+router.get('/', getWorkouts);
 
 // GET a single workout
-router.get('/:id', getWorkout)
+router.get('/:id', getWorkout);
 
 //POST a new workout
-router.post('/', createWorkout )
+router.post('/', createWorkout );
 
 //DELETE a new workout
-router.delete('/:id', deleteWorkout)
+router.delete('/:id', deleteWorkout);
 
 //UPDATE a new workout
-router.patch('/:id', updateWorkout)
+router.patch('/:id', updateWorkout);
 
 
 
-module.exports = router
+module.exports = router;
